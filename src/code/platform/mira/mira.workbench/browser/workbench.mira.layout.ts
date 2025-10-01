6031e847db749d33c544fdbd4f67c0a6d334347e
Siddarth <sidzy4@gmail.com>
2025-10-01 21:49:07 +0530

    Removed useless lines

--- src/code/platform/mira/mira.workbench/browser/workbench.mira.layout.ts
+++ src/code/platform/mira/mira.workbench/browser/workbench.mira.layout.ts
@@ -17,6 +17,8 @@ export class Mira extends CoreEl {
     // _cursor.innerHTML = getIcon("cursor/cursor.svg");
     // _cursor.className = "cursor";
 
+    // document.body.appendChild(_cursor);
+
     const scale = document.createElement("div");
     scale.className = "scale-0";
 
@@ -38,11 +40,8 @@ export class Mira extends CoreEl {
     this._el.appendChild(transcriptionText);
     this._el.appendChild(scale);
 
-    // document.body.appendChild(_cursor);
-
     setTimeout(async () => {
-      // _mic._start();
-      // _voice.say("Welcome to Meridia, A new world of programming.");
+      _mic._start();
     }, 100);
   }
 }
